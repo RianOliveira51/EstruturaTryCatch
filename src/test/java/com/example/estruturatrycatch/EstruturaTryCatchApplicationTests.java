645b436df7d74729ff110e1f7adf7e9fc8af2431
Rian <rian.nair2017@gmail.com>
2024-08-02 01:15:18 -0300

    Criando primeira versão do codigo

---
+++ src/test/java/com/example/estruturatrycatch/EstruturaTryCatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estruturatrycatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstruturaTryCatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
